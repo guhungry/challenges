21f6c9d2a2fbc10511ed0c6a5eea30b74c0f8126
Woraphot Chokratanasombat <guhungry@gmail.com>
2018-05-22 23:30:18 +0700

    [Colors] Change: Extract blueOmise

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,19 @@
 import React, { PureComponent } from 'react';
 
+import Colors from '../styles/Colors'
+
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
 const Container = styled.div`
-  border: 1px solid #00f;
-  color: #00f;
+  border: 1px solid ${Colors.blueOmise};
+  color: ${Colors.blueOmise};
   cursor: pointer;
   padding: 5px 15px;
   border-radius: 6px;
   &:hover {
     color: white;
-    background-color: #00f;
+    background-color: ${Colors.blueOmise};
   }
 `;
 
@@ -23,8 +25,7 @@ export default class Button extends PureComponent {
 
   render () {
     const { title, onClick } = this.props
-    return (
-      <Container onClick={onClick}>{title}</Container>
-    )
+
+    return <Container onClick={onClick}>{title}</Container>
   }
 }
