b24f92f6bea6ca9b53f515924298d9ee3563bd73
Woraphot Chokratanasombat <guhungry@gmail.com>
2018-05-23 17:51:36 +0700

    [yarn] Change: Move babel-plugin-transform-class-properties to devDependencies

--- src/helpers.js
+++
@@ -1,5 +0,0 @@
-export const donationAmount = donation => donation.amount || 0
-export const summaryDonations = (danations) => danations.reduce((accumulator, value) => (accumulator + value))
-
-export const noop = () => {}
-export const responseToJson = response => response.json()
