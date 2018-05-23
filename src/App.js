fff559d1d931ef9383bb7ca9704b60b1554c15d1
Woraphot Chokratanasombat <guhungry@gmail.com>
2018-05-23 12:33:12 +0700

    [App] Change: Extract mapDispatchToProps to remove duplicate dispatch

--- src/App.js
+++ src/App.js
@@ -24,87 +24,84 @@ const Message = styled.p`
   text-align: center;
 `;
 
-export default connect((state) => state)(
-  class App extends Component {
-    constructor(props) {
-      super();
+class App extends Component {
+  constructor(props) {
+    super();
 
-      this.state = {
-        charities: [],
-        selectedAmount: 10,
-      };
-    }
-
-    // ///////////////////
-    // Lifecycle Functions
-    // ///////////////////
-    componentDidMount() {
-      const self = this;
-      fetch('http://localhost:3001/charities')
-        .then(responseToJson)
-        .then(function(data) {
-          self.setState({ charities: data }) });
-
-      fetch('http://localhost:3001/payments')
-        .then(responseToJson)
-        .then(function(data) {
-          self.props.dispatch({
-            type: 'UPDATE_TOTAL_DONATE',
-            amount: summaryDonations(data.map((item) => (item.amount || 0))),
-          });
-        })
-    }
-
-    // ////////////////
-    // Render Functions
-    // ////////////////
-    render() {
-      const { donate, message } = this.props;
-      const { charities } = this.state
-
-      return (
-        <AppContainer>
-          <AppTitle>Omise Tamboon React</AppTitle>
-          <ResponsiveContainer>
-            <p>All donations: {donate}</p>
-            <Message>{message}</Message>
-            <ResponsiveRow gutterWidth='20px'>{ charities.map(this.renderCharity) }</ResponsiveRow>
-          </ResponsiveContainer>
-        </AppContainer>
-      );
-    }
-
-    renderCharity = (item, key) => <Card item={item} key={key} onPay={this.handlePay} />
-
-    // ////////////////
-    // Helper Functions
-    // ////////////////
-    handlePay = (id, amount, currency) => () => {
-      fetch('http://localhost:3001/payments', {
-        method: 'POST',
-        body: `{ "charitiesId": ${id}, "amount": ${amount}, "currency": "${currency}" }`,
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      })
-        .then(responseToJson)
-        .then(() => {
-          this.props.dispatch({
-            type: 'UPDATE_TOTAL_DONATE',
-            amount,
-          });
-          this.props.dispatch({
-            type: 'UPDATE_MESSAGE',
-            message: `Thanks for donate ${amount}!`,
-          });
-
-          setTimeout(() => {
-            this.props.dispatch({
-              type: 'UPDATE_MESSAGE',
-              message: '',
-            });
-          }, 2000);
-        });
-    }
+    this.state = {
+      charities: [],
+      selectedAmount: 10,
+    };
   }
-);
+
+  // ///////////////////
+  // Lifecycle Functions
+  // ///////////////////
+  componentDidMount() {
+    const self = this;
+    fetch('http://localhost:3001/charities')
+      .then(responseToJson)
+      .then(function(data) {
+        self.setState({ charities: data }) });
+
+    fetch('http://localhost:3001/payments')
+      .then(responseToJson)
+      .then(function(data) {
+        self.props.updateTotalDonate(summaryDonations(data.map(item => item.amount || 0)));
+      })
+  }
+
+  // ////////////////
+  // Render Functions
+  // ////////////////
+  render() {
+    const { donate, message } = this.props;
+    const { charities } = this.state
+
+    return (
+      <AppContainer>
+        <AppTitle>Omise Tamboon React</AppTitle>
+        <ResponsiveContainer>
+          <p>All donations: {donate}</p>
+          <Message>{message}</Message>
+          <ResponsiveRow gutterWidth='20px'>{ charities.map(this.renderCharity) }</ResponsiveRow>
+        </ResponsiveContainer>
+      </AppContainer>
+    );
+  }
+
+  renderCharity = (item, key) => <Card item={item} key={key} onPay={this.handlePay} />
+
+  // ////////////////
+  // Helper Functions
+  // ////////////////
+  handlePay = (id, amount, currency) => () => {
+    fetch('http://localhost:3001/payments', {
+      method: 'POST',
+      body: `{ "charitiesId": ${id}, "amount": ${amount}, "currency": "${currency}" }`,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+      .then(responseToJson)
+      .then(() => {
+        this.props.updateTotalDonate(amount);
+        this.props.updateMessage(`Thanks for donate ${amount}!`);
+
+        setTimeout(() => {
+          this.props.updateMessage('');
+        }, 2000);
+      });
+  }
+}
+
+const mapStateToProps = state => state
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateTotalDonate: amount => dispatch({ type: 'UPDATE_TOTAL_DONATE', amount }),
+    updateMessage: message => dispatch({ type: 'UPDATE_MESSAGE', message })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
