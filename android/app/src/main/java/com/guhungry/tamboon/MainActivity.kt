55368b595d125009570184548745abbec32d0f94
Woraphot Chokratanasombat <guhungry@gmail.com>
2018-05-24 21:31:10 +0700

    [Main Activity] Change: Add Donation View for test

--- android/app/src/main/java/com/guhungry/tamboon/MainActivity.kt
+++ android/app/src/main/java/com/guhungry/tamboon/MainActivity.kt
@@ -2,8 +2,12 @@ package com.guhungry.tamboon
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.guhungry.tamboon.dummy.DummyContent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DonationFragment.OnListFragmentInteractionListener {
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+        // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
