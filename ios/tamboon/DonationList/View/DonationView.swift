68e18a0ef8c088edbd85a3c9254b6417529b700f
Woraphot Chokratanasombat <guhungry@gmail.com>
2018-05-24 19:11:58 +0700

    [Donation View] Change: Add Donation View for Cell

---
+++ ios/tamboon/DonationList/View/DonationView.swift
@@ -0,0 +1,14 @@
+//
+//  DonationView.swift
+//  tamboon
+//
+//  Created by Woraphot Chokratanasombat on 24/5/2561 BE.
+//  Copyright © 2561 Woraphot Chokratanasombat. All rights reserved.
+//
+
+import UIKit
+
+class DonationView : UITableViewCell {
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
