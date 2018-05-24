94f4ea0af8ab2b8918ca67501c2e19fc2742c7bf
Woraphot Chokratanasombat <guhungry@gmail.com>
2018-05-24 18:15:10 +0700

    [Donation List] Change: Add blank donation cell

--- ios/tamboon/ViewController.swift
+++ ios/tamboon/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableDonation: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return self.tableDonation.dequeueReusableCell(withIdentifier: "DonationRow", for: indexPath)
+    }
 }
 
