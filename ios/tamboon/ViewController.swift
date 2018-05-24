70e6edf1f4a944a7d530d26456fec59909236d31
Woraphot Chokratanasombat <guhungry@gmail.com>
2018-05-24 17:27:52 +0700

    [iOS] Change: Import blank project

---
+++ ios/tamboon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tamboon
+//
+//  Created by Woraphot Chokratanasombat on 24/5/2561 BE.
+//  Copyright © 2561 Woraphot Chokratanasombat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
